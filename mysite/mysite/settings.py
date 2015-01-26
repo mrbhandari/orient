6015444d1e2e9d5caff7b55e7c14e4a92ff1e4d5
Rahul Bhandari <rahul@bloomreach.com>
2015-01-25 17:46:34 -0800

    basic settings updated

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -58,10 +58,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'orient',
+            #os.path.join(PROJECT_ROOT, 'mysql.db'),                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'thebakery',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
