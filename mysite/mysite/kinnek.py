344b4338ef5ef16c4708bb0242cbf592941cf8c5
pradeep <pradeep.muthukrishnan@gmail.com>
2015-02-04 00:07:30 -0800

    Adding field names to define event signatures

--- mysite/mysite/kinnek.py
+++ mysite/mysite/kinnek.py
@@ -185,7 +185,8 @@ UNION SELECT failure_uids_events_cnt.* FROM failure_uids_events_cnt;
     prev_uid = ""
     prev_conv = 0
     agg_event_ctr = {}
-    for row in cur.fetchall():
+    column_names = cur.description
+    for row in cur.fetchall(): 
         print "OUTPUT:" +  str(row)
         uid = row[0]
         if prev_uid != uid:
@@ -216,13 +217,20 @@ UNION SELECT failure_uids_events_cnt.* FROM failure_uids_events_cnt;
         event_signatures = set()
         event_signature_str = ""
         for i in range(1,len(row) - 2):
-            event_signature_str = event_signature_str + " " + str(row[i])
-            event_signature_temp = str(row[i])
+            field_name = column_names[i][0]
+            if str(row[i]).strip() == "":
+                continue
+            if event_signature_str == "":
+                event_signature_str = field_name+ "=" + str(row[i])
+            else:
+                event_signature_str = event_signature_str + " " + field_name + "=" + str(row[i])
+            event_signature_temp = field_name + "=" + str(row[i])
             if event_signature_temp:
                 if event_signature_temp in agg_event_ctr:
                     agg_event_ctr[event_signature_temp] = agg_event_ctr[event_signature_temp] + row[-2]
                 else:
                     agg_event_ctr[event_signature_temp] = row[-2]
+        print "EVENT SIGNATURE",event_signature_str
         event_signatures.add(event_signature_str)
         for event_signature in event_signatures:
             if event_signature in event_metadata:
